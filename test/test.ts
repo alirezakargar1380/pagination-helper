2a73ebdfa2355ef20fb6b4c3fcc9eb25dc770f8f
alireza kargar <imanesche@gmail.com>
2022-12-27 17:58:29 +0330

    update test

--- test/test.ts
+++ test/test.ts
@@ -1,9 +1,9 @@
 import pagination from "./../index";
 
 const p = new pagination({
-    data_per_page: 2,
-    exportDataAsarray: true
+    data_per_page: 10,
+    exportDataAsarray: false
 })
 // console.log(p.getTakeAndSkip(2))
-console.log(p.getNumberOfPages(10))
+console.log(p.getNumberOfPages(20.9))
 // console.log(p.getPageNumberByOffsetAndLimit(30, 10))
